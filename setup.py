78c3edc43ff6a278cb9e671ba4393a9b1badfead
Ben Mabey <ben@benmabey.com>
2015-04-06 17:46:03 -0600

    adds py.test

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 
 try:
     from setuptools import setup
+    from setuptools.command.test import test as TestCommand
 except ImportError:
     from distutils.core import setup
+    from distutils.core.command.test import test as TestCommand
 
 
 with open('README.rst') as readme_file:
@@ -14,12 +17,33 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
+# via https://pytest.org/latest/goodpractises.html
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 requirements = [
-    # TODO: put package requirements here
+    'pandas',
+    'numpy',
+    'scikit-bio==0.2.3'
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest'
 ]
 
 setup(
@@ -43,7 +67,8 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
@@ -53,5 +78,6 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    cmdclass = {'test': PyTest}
 )
---
+++ tests/pyLDAvis/test_prepare.py
@@ -0,0 +1,8 @@
+import pyLDAvis.prepare as p
+
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
