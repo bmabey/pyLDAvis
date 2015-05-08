f7eef17edb2dea7e6fac48fa633be43ebfd2a271
Ben Mabey <ben@benmabey.com>
2015-05-08 11:16:06 -0600

    import all the save* functions into main module

--- pyLDAvis/__init__.py
+++ pyLDAvis/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Topic Models (LDA) visualization using D3
+Topic Models (e.g. LDA) visualization using D3
 =============================================
 
 Functions: General Use
@@ -9,16 +9,16 @@ Functions: General Use
     transform and prepare a LDA model's data for visualization
 
 :func:`prepared_data_to_html`
-    convert prepared data to a html string
+    convert prepared data to an html string
 
 :func:`show`
-    launch a web server to view the d3/html visualization
+    launch a web server to view the d3 visualization
 
 :func:`save_html`
     save a visualization to a standalone html file
 
 :func:`save_json`
-    save a JSON representation of a figure to file
+    save the visualization JSON data of to a file
 
 
 Functions: IPython Notebook
@@ -35,7 +35,7 @@ Functions: IPython Notebook
 
 __all__ = ["__version__",
            "prepare", "prepared_data_to_html",
-           "display", "show", #"save_html", "save_json",
+           "display", "show", "save_html", "save_json",
            "enable_notebook", "disable_notebook"]
 
 __version__ = '0.1.0-git'
