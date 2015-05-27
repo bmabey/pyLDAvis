dadbaaaca7941d39d6e5c8829a2a543fa18b4de0
Ben Mabey <ben@benmabey.com>
2015-05-27 14:10:45 -0600

    remove use of xrange to make python 3.x compat

--- pyLDAvis/prepare.py
+++ pyLDAvis/prepare.py
@@ -89,7 +89,7 @@ def _topic_coordinates(mds, topic_term_dists, topic_proportion):
 def _chunks(l, n):
     """ Yield successive n-sized chunks from l.
     """
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i+n]
 
 def _job_chunks(l, n_jobs):
