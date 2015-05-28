d74384bc5d27dc78c496ab4d0aa177fb307c6cc4
Ben Mabey <ben@benmabey.com>
2015-05-28 15:33:05 -0600

    trying another method to get numpydocs to the styles show up..

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import mock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('sphinxext'))
+#sys.path.insert(0, os.path.abspath('.'))
 
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
@@ -54,10 +54,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
-
-import numpy_ext.numpydoc
-extensions.append('numpy_ext.numpydoc')
+    'sphinx.ext.viewcode',
+    'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
