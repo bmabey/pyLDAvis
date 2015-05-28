97685fb24f43cc028abb3435f8a3d0110d9751a3
Ben Mabey <ben@benmabey.com>
2015-05-28 14:59:02 -0600

    trying to use numpydoc ext

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,13 @@ import pyLDAvis
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'numpy_ext.numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
