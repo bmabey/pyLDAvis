2a55dfccca0ae9ff84df2b4977a6fb2e0b2e062c
Ben Mabey <ben@benmabey.com>
2015-06-02 19:12:28 -0600

    change how web assets are served from github.. using detatched branch

--- pyLDAvis/urls.py
+++ pyLDAvis/urls.py
@@ -13,21 +13,31 @@ __all__ = ["D3_URL", "LDAVIS_URL", "LDAVISMIN_URL", "LDAVIS_CSS_URL",
 
 D3_URL = "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"
 
-# for dev
-#WWW_JS_DIR = "https://rawgit.com/bmabey/pyLDAvis/master/pyLDAvis/js/"
-# for stable releases
-WWW_JS_DIR = "https://cdn.rawgit.com/bmabey/pyLDAvis/master/pyLDAvis/js/"
-
-LDAVIS_URL = WWW_JS_DIR + "ldavis.v{0}.js".format(__version__)
-LDAVISMIN_URL = LDAVIS_URL
-LDAVIS_CSS_URL = WWW_JS_DIR + "ldavis.v{0}.css".format(__version__)
-
+DEV = 'git' in __version__
 LOCAL_JS_DIR = os.path.join(__path__[0], "js")
 D3_LOCAL = os.path.join(LOCAL_JS_DIR, "d3.v3.min.js")
-LDAVIS_LOCAL = os.path.join(LOCAL_JS_DIR,
-                           "ldavis.v{0}.js".format(__version__))
 
-LDAVIS_CSS_LOCAL = os.path.join(LOCAL_JS_DIR,
-                           "ldavis.v{0}.css".format(__version__))
-LDAVISMIN_LOCAL = os.path.join(LOCAL_JS_DIR,
-                              "ldavis.v{0}.min.js".format(__version__))
+if DEV:
+    WWW_JS_DIR = "https://rawgit.com/bmabey/pyLDAvis/master/pyLDAvis/js/"
+    LDAVIS_URL = WWW_JS_DIR + "ldavis.js"
+    LDAVIS_CSS_URL = WWW_JS_DIR + "ldavis.css"
+
+    LDAVIS_LOCAL = os.path.join(LOCAL_JS_DIR, "ldavis.js")
+
+    LDAVIS_CSS_LOCAL = os.path.join(LOCAL_JS_DIR, "ldavis.css")
+else:
+    WWW_JS_DIR = "https://cdn.rawgit.com/bmabey/pyLDAvis/files/"
+    JS_VERSION = '1.0.0'
+    CSS_VERSION = '1.0.0'
+
+    LDAVIS_URL = WWW_JS_DIR + "ldavis.v{0}.js".format(JS_VERSION)
+    LDAVIS_CSS_URL = WWW_JS_DIR + "ldavis.v{0}.css".format(CSS_VERSION)
+
+    LDAVIS_LOCAL = os.path.join(LOCAL_JS_DIR,
+                           "ldavis.v{0}.js".format(JS_VERSION))
+
+    LDAVIS_CSS_LOCAL = os.path.join(LOCAL_JS_DIR,
+                           "ldavis.v{0}.css".format(CSS_VERSION))
+
+LDAVISMIN_URL = LDAVIS_URL
+LDAVISMIN_LOCAL = LDAVIS_LOCAL
