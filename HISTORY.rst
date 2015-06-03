5315a59ea56c7d63dad4a5d585011e2c42736f0c
Ben Mabey <ben@benmabey.com>
2015-06-02 19:43:23 -0600

    releases v1.1.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,7 +3,12 @@
 History
 -------
 
-0.1.0 (2015-05-29)
+1.1.0 (2015-06-02)
+---------------------
+
+* Fixes bug with GraphLab function that was producing bogus visualizations.
+
+1.0.0 (2015-05-29)
 ---------------------
 
 * First release on PyPI. Faithful port of R version with IPython support and helper functions for GraphLab & gensim.
--- setup.py
+++ setup.py
@@ -61,13 +61,13 @@ test_requirements = [
 
 setup(
     name='pyLDAvis',
-    version='1.0.0',
+    version='1.1.0',
     description="Interactive topic model visualization. Port of the R package.",
     long_description=readme + '\n\n' + history,
     author="Ben Mabey",
     author_email='ben@benmabey.com',
     url='https://github.com/bmabey/pyLDAvis',
-    download_url = 'https://github.com/bmabey/pyLDAvis/tarball/1.0.0',
+    download_url = 'https://github.com/bmabey/pyLDAvis/tarball/1.1.0',
     packages=[
         'pyLDAvis',
     ],
