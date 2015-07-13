209edc05e342df02989d6d27807beca7da0f3000
Ben Mabey <ben@benmabey.com>
2015-07-12 20:59:46 -0600

    use the graphlab API for it's prepare, 3x+ speed improvement!

--- pyLDAvis/graphlab.py
+++ pyLDAvis/graphlab.py
@@ -4,29 +4,44 @@ pyLDAvis GraphLab
 Helper functions to visualize GraphLab Create's TopicModel (an implementation of LDA)
 """
 
+from __future__ import absolute_import
+
 import funcy as fp
 import numpy as np
 import pandas as pd
-from . import prepare as vis_prepare
+import graphlab as gl
+import pyLDAvis
 
 def _topics_as_df(topic_model):
     tdf = topic_model['topics'].to_dataframe()
     return pd.DataFrame(np.vstack(tdf['topic_probabilities'].values), index=tdf['vocabulary'])
 
+def _sum_sarray_dicts(sarray):
+    counts_sf = gl.SFrame({'count_dicts': sarray}).stack('count_dicts').groupby(key_columns='X1',
+                                              operations={'count': gl.aggregate.SUM('X2')})
+    return counts_sf.unstack(column=['X1', 'count'])[0].values()[0]
+
+def _extract_doc_data(docs):
+    doc_lengths = list(docs.apply(lambda d: np.array(d.values()).sum()))
+    term_freqs_dict = _sum_sarray_dicts(docs)
+
+    vocab = term_freqs_dict.keys()
+    term_freqs = term_freqs_dict.values()
+
+    return {'doc_lengths': doc_lengths, 'vocab': vocab, 'term_frequency': term_freqs}
+
+def _extract_model_data(topic_model, docs, vocab):
+    doc_topic_dists = np.vstack(topic_model.predict(docs, output_type='probabilities'))
+
+    topics = _topics_as_df(topic_model)
+    topic_term_dists = topics.T[vocab].values
+
+    return {'topic_term_dists': topic_term_dists, 'doc_topic_dists': doc_topic_dists}
+
 def _extract_data(topic_model, docs):
-   doc_lengths = [np.array(d.values()).sum() for d in docs]
-
-   term_freqs_dict = fp.merge_with(sum, *docs)
-   vocab = term_freqs_dict.keys()
-   term_freqs = term_freqs_dict.values()
-
-   doc_topic_dists = np.vstack(topic_model.predict(docs, output_type='probabilities'))
-
-   topics = _topics_as_df(topic_model)
-   topic_term_dists = topics.T[vocab].values
-
-   return {'topic_term_dists': topic_term_dists, 'doc_topic_dists': doc_topic_dists,
-           'doc_lengths': doc_lengths, 'vocab': vocab, 'term_frequency': term_freqs}
+    doc_data = _extract_doc_data(docs)
+    model_data = _extract_model_data(topic_model, docs, doc_data['vocab'])
+    return fp.merge(doc_data, model_data)
 
 def prepare(topic_model, docs, **kargs):
     """Transforms the GraphLab TopicModel and related corpus data into
@@ -52,4 +67,4 @@ def prepare(topic_model, docs, **kargs):
     http://nbviewer.ipython.org/github/bmabey/pyLDAvis/blob/master/notebooks/GraphLab.ipynb
     """
     opts = fp.merge(_extract_data(topic_model, docs), kargs)
-    return vis_prepare(**opts)
+    return pyLDAvis.prepare(**opts)
