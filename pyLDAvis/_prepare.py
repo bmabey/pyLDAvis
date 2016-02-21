4443592ffe91fd25f4cfa8ccc788fa793cabc2c4
Ben Mabey <ben@benmabey.com>
2016-02-20 21:06:26 -0700

    removes _term_topic_freq, no need to deprecate a private fn

--- pyLDAvis/_prepare.py
+++ pyLDAvis/_prepare.py
@@ -239,13 +239,6 @@ def _token_table(topic_info, term_topic_freq, vocab, term_frequency):
    token_table['Freq'] = token_table.Freq / term_frequency[token_table.index]
    return token_table.sort_values(by=['Term', 'Topic'])
 
-# Ths method is now deprecated and replaced by code in the prepare method.
-def _term_topic_freq(topic_term_dists, topic_freq, term_frequency):
-   term_topic_freq = (topic_term_dists.T  * topic_freq).T
-   # adjust to match term frequencies exactly (get rid of rounding error)
-   err = term_frequency / term_topic_freq.sum()
-   return term_topic_freq * err
-
 
 def prepare(topic_term_dists, doc_topic_dists, doc_lengths, vocab, term_frequency, \
             R=30, lambda_step=0.01, mds=js_PCoA, n_jobs=-1, \
@@ -339,20 +332,15 @@ def prepare(topic_term_dists, doc_topic_dists, doc_lengths, vocab, term_frequenc
    topic_freq       = topic_freq[topic_order]
    topic_term_dists = topic_term_dists.ix[topic_order]
    doc_topic_dists  = doc_topic_dists[topic_order]
-   
+
    # token counts for each term-topic combination (widths of red bars)
-   ## Moving this line from _term_topic_freq method, which is deprecated since
-   ## we no longer need to adjust to match term frequencies exactly by avoiding
-   ## rounding error.
    term_topic_freq = (topic_term_dists.T * topic_freq).T
-   ## Quick fix for red bar width bug.  We calculate the 
-   ## term frequencies internally, using the topic term distributions and the 
+   ## Quick fix for red bar width bug.  We calculate the
+   ## term frequencies internally, using the topic term distributions and the
    ## topic frequencies, rather than using the user-supplied term frequencies.
    ## For a detailed discussion, see: https://github.com/cpsievert/LDAvis/pull/41
    term_frequency = np.sum(term_topic_freq, axis=0)
 
-   # Removing call to _term_topic_freq.  See note above about term_topic_freq.
-   #term_topic_freq    = _term_topic_freq(topic_term_dists, topic_freq, term_frequency)
    topic_info         = _topic_info(topic_term_dists, topic_proportion, term_frequency, term_topic_freq, vocab, lambda_step, R, n_jobs)
    token_table        = _token_table(topic_info, term_topic_freq, vocab, term_frequency)
    topic_coordinates = _topic_coordinates(mds, topic_term_dists, topic_proportion)
