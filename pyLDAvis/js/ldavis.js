7bba733d37951724b23cc0484e8eb2790b8ca740
Partho Mandal <partho.iiitm@gmail.com>
2016-11-13 12:35:29 -0700

    Trying to pass multiple json data in ldavis.js

--- pyLDAvis/js/ldavis.js
+++ pyLDAvis/js/ldavis.js
@@ -1397,18 +1397,10 @@ var LDAvis = function(to_select, data_or_file_name_list) {
 
     }
 
-    // if (typeof data_or_file_name_a === 'string' && typeof data_or_file_name_b === 'string')
-    //     d3.json(data_or_file_name_a, function(error, data) {visualize(data, 1);});
-    //     d3.json(data_or_file_name_b, function(error, data) {visualize(data, 2);});
-    // else
-    //     visualize(data_or_file_name_a, 1);
-    //     visualize(data_or_file_name_b, 2);
-
-    if (typeof data_or_file_name_list === 'string'){
-        d3.json(data_or_file_name_list, function(error, data) {visualize(data);});
-    }
+    if (typeof data_or_file_name_list[0] === 'string')
+        d3.json(data_or_file_name_list[0], function(error, data) {visualize(data);});
     else
-          visualize(data_or_file_name_list);
+        visualize(data_or_file_name_list[0]);
 
     // var current_clicked = {
     //     what: "nothing",
