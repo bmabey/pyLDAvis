3cf8b57f3c242722046712de937a0f71d652c996
Ben Mabey <ben@benmabey.com>
2016-11-29 13:11:13 -0700

    fix term sorting

--- pyLDAvis/_prepare.py
+++ pyLDAvis/_prepare.py
@@ -264,7 +264,7 @@ def _token_table(topic_info, term_topic_freq, vocab, term_frequency):
 
    # term-topic frequency table of unique terms across all topics and all values of lambda
    term_ix = topic_info.index.unique()
-   term_ix = np.sort(term_ix.values)
+   term_ix = np.sort(term_ix)
 
    top_topic_terms_freq = term_topic_freq[term_ix]
    # use the new ordering for the topics
