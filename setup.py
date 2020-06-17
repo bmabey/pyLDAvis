c92f0aa5a34cfc31ae9bcb97077e8fc19319fa11
ZechyW <ZechyW@users.noreply.github.com>
2020-06-17 17:20:09 +0800

    Optimised calculations to work with later `pandas` versions
    - Removed mutable default `plot_opts` from `.prepare()`
    - Construct `topic_term_dists` DataFrame by column rather than by
      row
    - Replaced a number of bottlenecked operations with `numexpr`
      versions using `pd.eval()`
    - Fixed pandas FutureWarning and related tests (#132, #133)
    - Fixed IPython shim warning for locally displayed visualisations
    - Updated Travis test configuration

--- setup.py
+++ setup.py
@@ -1,43 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
 import os
 
-
-try:
-    from setuptools import setup
-    from setuptools.command.test import test as TestCommand
-except ImportError:
-    from distutils.core import setup
-    from distutils.core.command.test import test as TestCommand
-
+from setuptools import setup
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
-
-# via https://pytest.org/latest/goodpractises.html
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
@@ -47,16 +16,9 @@ else:
     with open('requirements.txt') as f:
         requirements = f.read().splitlines()
 
-
-test_requirements = [
-    'pytest',
-    'funcy',
-    'gensim'
-]
-
 setup(
     name='pyLDAvis',
-    version='2.1.3',
+    version='2.1.4',
     description="Interactive topic model visualization. Port of the R package.",
     long_description=readme + '\n\n' + history,
     author="Ben Mabey",
@@ -80,13 +42,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    cmdclass = {'test': PyTest}
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
