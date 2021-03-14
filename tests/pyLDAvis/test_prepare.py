a02e6074aa7845e78e1f88b0c3fe283eb26ad24a
Mark Susoil <marksusol@gmail.com>
2021-03-14 11:53:38 -0600

    FutureWarning: pandas.util.testing is deprecated

--- tests/pyLDAvis/test_prepare.py
+++ tests/pyLDAvis/test_prepare.py
@@ -9,7 +9,7 @@ import funcy as fp
 from numpy.testing import assert_array_equal
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from pyLDAvis import prepare
 
